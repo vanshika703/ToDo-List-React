30b1781f83433c3afdf106d7f0e637839c5abf07
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-03-28 17:01:26 +0530

    feat : add and delete in to do app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import WorkItem from "./components/WorkItem";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleAdd = () => {
+    setItems((prev) => [...prev, item]);
+  };
+
+  const handleDelete = (index) => {
+    const data = [...items];
+    data.splice(index, 1);
+    setItems(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>To DO</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="new task"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button onClick={handleAdd}>Add Task</button>
+        {console.log("items", items)}
+      </div>
+      {items.map((item, i) => (
+        <div key={i}>
+          <WorkItem item={item} />
+          <button onClick={() => handleDelete(i)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/components/WorkItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const workItem = ({ item }) => {
+  return <div>{item}</div>;
+};
+
+export default workItem;
